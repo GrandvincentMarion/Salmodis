6f666e5d501b5fca45f132397bef1c32fbad34c6
Marion <grandvincent.marion@gmail.com>
2018-03-02 00:50:05 +0100

    update 02/03/2018

--- js/script.js
+++ js/script.js
@@ -103,6 +103,8 @@ $(document).ready(function() {
     }
     MenuLateral();
     
+    
+
 });
 
 
@@ -230,3 +232,30 @@ function formulaire() {
 
 }
 formulaire()
+
+
+$(document).ready(function() {
+	$('.gallery-content').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		closeOnContentClick: false,
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		image: {
+			verticalFit: true,
+			titleSrc: function(item) {
+				return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
+			}
+		},
+		gallery: {
+			enabled: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300, // don't foget to change the duration also in CSS
+			opener: function(element) {
+				return element.find('img');
+			}
+		}
+    });
+});
